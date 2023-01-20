62ada929c5fbb00d5e825d854fe60e2f607c9fe0
Diego Gonzales <diegoegj14@gmail.com>
2023-01-20 17:51:19 -0500

    App mouse follower finished

--- projects/03-mouse-follower/src/App.tsx
+++ projects/03-mouse-follower/src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { MousePosition } from "./models/mouse-position.interface";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState<boolean>(false);
+  const [position, setPosition] = useState<MousePosition>({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handlePointerMove = (event: PointerEvent) => {
+      const newPosition = { ...position };
+      newPosition.x = event.clientX;
+      newPosition.y = event.clientY;
+
+      setPosition(newPosition);
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handlePointerMove);
+    }
+
+    // This is executed:
+    // Before the component is unmounted.
+    // When the dependencies change, before running the effect again.
+    return () => {
+      window.removeEventListener("pointermove", handlePointerMove);
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          display: enabled ? 'block' : 'none'
+        }}
+      ></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Disabled" : "Enabled"} mouse following
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
